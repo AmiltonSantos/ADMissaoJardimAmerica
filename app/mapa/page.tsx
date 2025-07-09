e8e1386eed8505a8b5a4e8a86d7f56a41d8cf558
Amilton Santos <a1000ton1000ton@gmail.com>
2025-07-09 11:50:54 -0300

    :fire: ajusta localizacao das congregacõe parte 3

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -15,15 +15,17 @@ const MapaPage = () => {
 
   // Defina o ícone de cada filial
   const filialLocations = [
-    { id: 1, nome: 'AD Missão Jardim América - Sede', lat: -16.719597545737017, lng: -49.29065610808544, icon: '/images/point.png' },
-    { id: 2, nome: 'AD Missão Jardim América - Setor dos Afonsos', lat: -16.73961881308889, lng: -49.26769419144868, icon: '/images/point.png' },
-    { id: 3, nome: 'Assembléia De Deus Ministério Missão Faiçalville', lat: -16.744697311090025, lng: -49.31881795557049, icon: '/images/point.png' },
-    { id: 4, nome: 'AD Missão Jardim América - Eli Forte', lat: -16.73361351758015, lng: -49.35228825863339, icon: '/images/point.png' },
-    { id: 5, nome: 'AD Missão Jardim América - Norte Ferroviário', lat: -16.654161360809066, lng: -49.26199790822422, icon: '/images/point.png' },
-    { id: 6, nome: 'AD Missão Jardim América - Recanto do Bosque', lat: -16.60319693257024, lng: -49.30436611189596, icon: '/images/point.png' },
-    { id: 7, nome: 'AD Missão Jardim América - Boa Vista', lat: -16.587073731678846, lng: -49.341654095493595, icon: '/images/point.png' },
-    { id: 8, nome: 'AD Missão Jardim América - 14 Bis', lat: -16.635386191536497, lng: -49.3989898523177, icon: '/images/point.png' },
-    { id: 9, nome: 'AD Missão Jardim América - João Braz', lat: -16.688402737957688, lng: -49.35104938908731, icon: '/images/point.png' },
+    { id: 1, nome: 'AD Missão Jardim América - Sede', lat: -16.719597545737017, lng: -49.29065610808544, icon: '/images/location/point-sede.png' },
+    { id: 2, nome: 'AD Missão Jardim América - Setor dos Afonsos', lat: -16.73961881308889, lng: -49.26769419144868, icon: '/images/location/point.png' },
+    { id: 3, nome: 'Assembléia De Deus Ministério Missão Faiçalville', lat: -16.744697311090025, lng: -49.31881795557049, icon: '/images/location/point.png' },
+    { id: 4, nome: 'AD Missão Jardim América - Eli Forte', lat: -16.73361351758015, lng: -49.35228825863339, icon: '/images/location/point.png' },
+    { id: 5, nome: 'AD Missão Jardim América - Norte Ferroviário', lat: -16.654161360809066, lng: -49.26199790822422, icon: '/images/location/point.png' },
+    { id: 6, nome: 'AD Missão Jardim América - Recanto do Bosque', lat: -16.60319693257024, lng: -49.30436611189596, icon: '/images/location/point.png' },
+    { id: 7, nome: 'AD Missão Jardim América - Boa Vista', lat: -16.587073731678846, lng: -49.341654095493595, icon: '/images/location/point.png' },
+    { id: 8, nome: 'AD Missão Jardim América - 14 Bis', lat: -16.635386191536497, lng: -49.3989898523177, icon: '/images/location/point.png' },
+    { id: 9, nome: 'AD Missão Jardim América - João Braz', lat: -16.688402737957688, lng: -49.35104938908731, icon: '/images/location/point.png' },
+    { id: 10, nome: 'AD Missão Jardim América - Buriti Sereno', lat: -16.77265034505131, lng: -49.31031942114354, icon: '/images/location/point.png' },
+    { id: 11, nome: 'Assembleia de Deus missão - Veiga Jardim', lat: -16.78524699420472, lng: -49.27453895787004, icon: '/images/location/point.png' },
   ];
 
   const [selectedFilial, setSelectedFilial] = useState<number | null>(null);
